6b5fac247092a3e44d09134d2ac314466cc8fee4
eethan1 <call132call132@gmail.com>
2018-07-27 15:15:24 +0800

    upgrade

--- js/link2SpreadSheet.js
+++ js/link2SpreadSheet.js
@@ -1,19 +1,21 @@
 function send2SpreadSheet(){
-    var name    = $("#name").val();
-    var email   = $("#email").val();
-    var phone   = $("#phone").val();
-    var message = $("#message").val();
+    // var name    = $("#name").val();
+    // var email   = $("#email").val();
+    // var phone   = $("#phone").val();
+    var name    = 'nnn';
+    var email   = 'eee';
+    var phone   = 'ppp';
     var data = {
-        "name": name,
-        "email": email,
-        "phone": phone,
-        "message": message,      
+        name: name,
+        email: email,
+        phone: phone,
     }
     console.log(data);
     $.ajax({
     type: "get",
     url: "https://script.google.com/macros/s/AKfycbyzB17Yt4RtuBfuh48X3xb_Xn8KjXfH0l9pGblWMalgk08ciXse/exec",
     data: data,
+    dataType: "JSON",
     success: function (response) {
             alert('success!!!');
         },
