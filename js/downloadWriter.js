a87b846781678259f6aedb3913e6e199e351acd2
eethan1 <call132call132@gmail.com>
2018-07-28 17:01:19 +0800

    new feature: download;finish simple download version

---
+++ js/downloadWriter.js
@@ -0,0 +1,12 @@
+listurl = 'https://script.google.com/macros/s/AKfycbxwtrgzfyKcI3wkopsmmOpz_ZYpWIRrzDBwbHGay4G4PnxYS6_f/exec'
+$.ajax({
+    type: "GET",
+    url: listurl,
+    success: function(){
+        
+
+
+    }
+}
+
+)
